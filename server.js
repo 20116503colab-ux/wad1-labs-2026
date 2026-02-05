8b873c2aafb2216a38e989cf9ed44739ba82de67
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 <20116503.colab@gmail.com>
2026-02-05 16:36:14 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import logger from "./utils/logger.js";
+
+
+'use strict';
+
+import express from 'express';
+import routes from './routes.js'; 
+
+const app = express();
+const port = 3000;
+
+app.use("/", routes);
+
+app.listen(port, () => logger.info(`Your app is listening on port ${port}!`));
+
+
